493e6b520ee06b2a6bdcb974c27398283fbb3a1c
kievtechnoshop@gmail.com <swr83947>
2019-12-02 15:44:49 +0200

    Updates

---
+++ precache-manifest.5688498b3ea518a9c3eca4e1c576d108.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e121f4a076363ac73c156ff7dd3793f",
+    "url": "/goit-react_router/index.html"
+  },
+  {
+    "revision": "a34028018bd05de2c050",
+    "url": "/goit-react_router/static/js/2.b0e18329.chunk.js"
+  },
+  {
+    "revision": "be454a82605fdf996d68",
+    "url": "/goit-react_router/static/js/main.d1e6e922.chunk.js"
+  },
+  {
+    "revision": "e633a61a8e552eae84ca",
+    "url": "/goit-react_router/static/js/runtime-main.19954526.js"
+  }
+]);
